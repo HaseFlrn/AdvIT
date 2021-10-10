6f2b313f78de29ada5e0341885bbde24eb7880c2
HaseFlrn <florianhase12@gmail.com>
2021-10-10 21:47:01 +0200

    java-project-structure

---
+++ Aufgaben/Java-Aufgaben/src/Aufgabe8/MyThread1.java
@@ -0,0 +1,31 @@
+package Aufgabe8;
+
+public class MyThread1 extends Thread {
+
+  private int id;
+
+  public MyThread1(int id) {
+    this.id = id;
+  }
+
+  public void run() {
+    try {
+      Thread.sleep((int) Math.random() * 1000);
+    } catch (Exception e) {}
+    System.out.println("Hello World (ID=" + id + ")");
+  }
+
+  public static void main(String[] args) {
+    Thread[] t = new Thread[10];
+    for (int i = 1; i < 10; i++) {
+      t[i] = new MyThread1(i);
+      t[i].start();
+    }
+    for (int i = 1; i < 10; i++) {
+      try {
+        t[i].join();
+      } catch (Exception e) {}
+    }
+    System.out.println("Alle fertig");
+  }
+}
