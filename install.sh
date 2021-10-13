12c5110ab315a3c40ad65c7aacd19f665a0765cd
HaseFlrn <florianhase12@gmail.com>
2021-10-13 19:21:43 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+snap install spotify && snap install discord && snap install code-classic && snap install flutter && flutter update && apt install vlc && apt install firefox && apt install thunderbird
