05957997532844528c5b132acc03cfca38242935
HaseFlrn <florianhase12@gmail.com>
2021-10-13 19:23:15 +0200

    spotify in apt

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-snap install spotify && snap install discord && snap install code-classic && snap install flutter && flutter update && apt install vlc && apt install firefox && apt install thunderbird
+apt install spotify && snap install discord && snap install code-classic && snap install flutter && flutter update && apt install vlc && apt install firefox && apt install thunderbird
