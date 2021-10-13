d276dad7af5919bb119eb3e9c13989b09323ecd9
HaseFlrn <florianhase12@gmail.com>
2021-10-13 09:10:06 +0200

    kp

--- Testat/Testat1/src/TeilB/App.java
+++ Testat/Testat1/src/TeilB/App.java
@@ -4,7 +4,7 @@ public class App {
 
   public static void main(String[] args) {
     Weiche w = new Weiche(2);
-    Thread t[] = { new Lok(0, w, 1000), new Lok(1, w, 1000) };
+    Thread t[] = { new Lok(0, w, 950), new Lok(1, w, 1000) };
     for (Thread thread : t) {
       thread.start();
     }
