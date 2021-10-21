0f647deddb262e36a26510674dc39808e02dc610
HaseFlrn <florianhase12@gmail.com>
2021-10-21 20:28:07 +0200

    Aufgabe fertig

--- Testat/Testat2/src/App.java
+++
@@ -1,6 +0,0 @@
-import java.util.UUID;
-
-public class App {
-
-  public static void main(String[] args) throws Exception {}
-}
--- Testat/Testat2/src/MsgServer.java
+++ Testat/Testat2/src/MsgServer.java
@@ -5,14 +5,56 @@ import java.util.UUID;
 public class MsgServer {
 
   public static final int DEFAULT_PORT = 7777;
-  public static final String PATH_TO_DESKTOP = "~/Desktop/Messages/";
+  public static final String PATH_TO_DESTINATION =
+    "/home/florian/Desktop/Messages/";
+
+  public static boolean createAndWrite(String messageValue, String key) {
+    FileWriter writer = null;
+    File newFile = null;
+    //Erstelle Datei
+    try {
+      newFile = new File(PATH_TO_DESTINATION + key);
+      if (newFile.createNewFile()) {
+        //Speichere Nachricht
+        try {
+          writer = new FileWriter(PATH_TO_DESTINATION + key);
+          writer.write(messageValue);
+          writer.close();
+          return true;
+        } catch (IOException e) {
+          System.err.println(e);
+        }
+      }
+    } catch (IOException e) {
+      System.err.println(e);
+    }
+    return false;
+  }
+
+  public static String readFromFile(String key) {
+    BufferedReader fileReader = null;
+    String message = null;
+    try {
+      fileReader =
+        new BufferedReader(new FileReader(PATH_TO_DESTINATION + key));
+      try {
+        message = fileReader.readLine();
+        fileReader.close();
+      } catch (IOException e) {
+        System.err.println(e);
+        return message;
+      }
+    } catch (FileNotFoundException e) {
+      System.err.println(e);
+    }
+    return message;
+  }
 
   public static void main(String[] args) {
     int port = DEFAULT_PORT;
     BufferedReader networkIn = null;
     PrintWriter networkOut = null;
     Socket connection = null;
-    FileWriter writer = null;
     try {
       ServerSocket server = new ServerSocket(port);
       while (true) {
@@ -25,34 +67,39 @@ public class MsgServer {
           networkOut = new PrintWriter(connection.getOutputStream());
           String line = networkIn.readLine();
           String[] splittedLine = line.split(" ", 2); //trennt das erste Wort des Strings vom Rest
-          System.out.println(splittedLine[0] + " und " + splittedLine[1]);
           if (splittedLine[0].equals("SAVE")) {
-            String value = splittedLine[1];
+            String messageValue = splittedLine[1];
             String key = UUID.randomUUID().toString(); //generiert unique-ID
-            //Erstelle Datei
-            try {
-              File newFile = new File(PATH_TO_DESKTOP + key);
-              if (newFile.createNewFile()) {
-                networkOut.println("KEY " + key); // sendet OK und Schlüssel an Client
-              }
-            } catch (IOException e) {
-              e.printStackTrace();
+            if (createAndWrite(messageValue, key)) {
+              networkOut.println("KEY " + key); // sendet KEY und Schlüssel an Client
+              networkOut.flush();
+              networkOut.close();
             }
-            //Speichere Nachricht
-            try {
-              writer = new FileWriter(PATH_TO_DESKTOP + key);
-              writer.write(value);
-              writer.close();
-            } catch (IOException e) {
-              e.printStackTrace();
+          } else if (splittedLine[0].equals("GET")) {
+            String key = splittedLine[1];
+            String messageValue = readFromFile(key);
+            if (messageValue != null) {
+              networkOut.println("OK " + messageValue); // sendet OK und Schlüssel an Client
+              networkOut.flush();
+              networkOut.close();
+            } else {
+              networkOut.println("FAILED "); // sendet OK und Schlüssel an Client
+              networkOut.flush();
+              networkOut.close();
             }
+          } else {
+            networkOut.println("FAILED "); // sendet OK und Schlüssel an Client
+            networkOut.flush();
+            networkOut.close();
           }
         } catch (IOException e) {
           e.printStackTrace();
         } finally {
           try {
             if (connection != null) connection.close();
-          } catch (IOException e) {}
+          } catch (IOException e) {
+            System.err.println(e);
+          }
         }
       }
     } catch (IOException e) {
